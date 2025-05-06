8cb83c53b3ecea7f1021e942770cce24b8072f76
Colin Lee <29541209+mistygibbon@users.noreply.github.com>
2025-05-06 17:28:26 -0400

    uibuild

--- backend/index.js
+++ backend/index.js
@@ -1,91 +1,103 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
-const cors = require('cors')
 
-const requestLogger = (request, response, next) => {
-    console.log('Method:', request.method)
-    console.log('Path:  ', request.path)
-    console.log('Body:  ', request.body)
-    console.log('---')
-    next()
-  }
+morgan.token('content', (request, response)=>{
+    const string = JSON.stringify(request.body)
+    if (request.method==="POST"){
+        return string ? string : ''
+    }
+    return ''
+})
 
 app.use(express.static('dist'))
-app.use(cors())
 app.use(express.json())
-app.use(requestLogger)
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :content'))
 
-let notes = [
-  {
-    id: "1",
-    content: "HTML is easy",
-    important: true
-  },
-  {
-    id: "2",
-    content: "Browser can execute only JavaScript",
-    important: false
-  },
-  {
-    id: "3",
-    content: "GET and POST are the most important methods of HTTP protocol",
-    important: true
-  }
+let entries = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
 ]
 
-app.get('/',(request,response) => {
-    response.send('<h1>Hello World!</h1>')
+// app.get('/', (request, response) => {
+//   response.send('<h1>Hello World!</h1>')
+// })
+
+app.get('/api/persons', (request, response) => {
+  response.json(entries)
 })
 
-app.get('/api/notes', (request, response) => {
-    response.json(notes)
+app.get('/info',(request,response)=>{
+    response.send(`Phonebook has info for ${entries.length} people <br\> ${new Date(Date.now()).toString()}`)
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/persons/:id',(request,response)=> {
     const id = request.params.id
-    const note = notes.find(note => note.id===id)
-    if (note) {
-        response.json(note)
+    const entry = entries.find(entry=>entry.id===id)
+    if (entry){
+        response.json(entry)
     } else {
         response.status(404).end()
     }
 })
 
-app.delete('/api/notes/:id', (request, response) => {
+app.delete('/api/persons/:id',(request,response)=>{
     const id = request.params.id
-    notes = notes.filter(note => note.id !== id)
-  
+    entries = entries.filter(entry=>entry.id!==id)
     response.status(204).end()
-  })
-
-const generateId = () => {
-  const maxId = notes.length > 0 ? Math.max(...notes.map(n=>Number(n.id))) : 0
-  return String(maxId+1)
-}
-
-app.post('/api/notes', (request, response) => {
-  const body = request.body
-
-  if (!body.content) {
-    return response.status(400).json({error: 'content missing'})
-  }
-
-  const note = {
-    content: body.content,
-    important: body.important || false,
-    id: generateId()
-  }
-  notes = notes.concat(note)
-  response.json(note)
 })
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
+const generateId = () => {
+    const maxId = entries.length>0 ? Math.max(...entries.map(entry=>Number(entry.id))) : 0
+
+    return String(maxId+1)
 }
 
-app.use(unknownEndpoint)
+app.post('/api/persons',(request,response)=>{
+    const body = request.body
+    // console.log(body)
+
+    if (!body.name) {
+        return response.status(400).json({error: 'name missing'})
+    }
+
+    if (!body.number) {
+        return response.status(400).json({error: 'number missing'})
+    }
+
+    if (entries.some(entry=>entry.name===body.name)){
+        return response.status(400).json({error: 'duplicate name'})
+    }
+
+    const entry = {
+        "id": generateId(),
+        "name": body.name,
+        "number": body.number,
+    }
+    entries = entries.concat(entry)
+    // console.log(entries)
+    response.json(entry)
+})
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })
